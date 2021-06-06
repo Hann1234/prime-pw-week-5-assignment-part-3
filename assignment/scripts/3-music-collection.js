1ab0a4dfd99bebad6c293488fae6d4fc76c6086c
Hann1234 <christopherjhannigan@gmail.com>
2021-06-06 09:57:23 -0500

    created addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,14 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+console.log(`My collection is: ${collection}`);
+
+function addToCollection(title, artist, yearPublished) {
+  let cd = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  }
+  collection.push(cd);
+  return cd;
+}
