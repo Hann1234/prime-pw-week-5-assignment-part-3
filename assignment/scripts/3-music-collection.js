f86ccc8163c705f573b84c3ff04cf8c6e5f83f71
Hann1234 <christopherjhannigan@gmail.com>
2021-06-06 10:45:16 -0500

    tested my addToCollection function and it works

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,7 +1,7 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
-console.log(`My collection is: ${collection}`);
+console.log('My collection is:', collection);
 
 function addToCollection(title, artist, yearPublished) {
   let cd = {
@@ -12,3 +12,11 @@ function addToCollection(title, artist, yearPublished) {
   collection.push(cd);
   return cd;
 }
+
+console.log('I am adding Stadium Arcadium by RHCP from 2006:', addToCollection('Stadium Arcadium', 'RHCP', '2006'));
+console.log('I am adding Californication by RHCP from 1999:', addToCollection('Californication', 'RHCP', '1999'));
+console.log('I am adding Abbey Road by The Beatles from 1969:', addToCollection('Abbey Road', 'The Beatles', '1969'));
+console.log('I am adding Let it Be by The Beatles from 1970:', addToCollection('Let it Be', 'The Beatles', '1970'));
+console.log('I am adding Lateralus by Tool from 2001:', addToCollection('Lateralus', 'Tool', '2001'));
+console.log('I am adding Reflektor by Arcade Fire from 2013:', addToCollection('Reflektor', 'Arcade Fire', '2013'));
+console.log('My collection is:', collection);
