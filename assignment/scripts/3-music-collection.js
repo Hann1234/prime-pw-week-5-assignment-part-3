ca8f2b1e1da477657064a4dce0d3bc9d995f8ab3
Hann1234 <christopherjhannigan@gmail.com>
2021-06-06 11:03:35 -0500

    finished and tested showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -19,4 +19,17 @@ console.log('I am adding Abbey Road by The Beatles from 1969:', addToCollection(
 console.log('I am adding Let it Be by The Beatles from 1970:', addToCollection('Let it Be', 'The Beatles', '1970'));
 console.log('I am adding Lateralus by Tool from 2001:', addToCollection('Lateralus', 'Tool', '2001'));
 console.log('I am adding Reflektor by Arcade Fire from 2013:', addToCollection('Reflektor', 'Arcade Fire', '2013'));
+
 console.log('My collection is:', collection);
+
+function showCollection(array) {
+  console.log('The number of items in the array is:', array.length);
+  for (let object of array) {
+    console.log('TITLE:', object.title);
+    console.log('ARTIST:', object.artist);
+    console.log('YEAR:', object.yearPublished);
+  }
+  return 'This is my collection.';
+}
+
+console.log('Test - showCollection:', showCollection(collection));
